6dcc3dd69a875b152567a16d95aa2317afd619e8
lybglory <lybglory@outlook.com>
2017-05-30 23:24:46 +0800

    GUISkin简单运用
    GUISkin简单运用：
    GUISkin有个方法GetStyle(string name)，这个方法的参数其实就是Custom style中的Name参数

---
+++ SomeUnityStudy/Assets/Scripts/GuiSkin.cs
@@ -0,0 +1,19 @@
+﻿/***
+* unity version:unity5.6.1f1
+* GUI Skin简单运用
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiSkin : MonoBehaviour {
+    public GUISkin btnGuiSkin;
+    public Texture2D texture2d_btn;
+    private void OnGUI()
+    {
+        GUI.skin = btnGuiSkin;
+        //GetStyle(stirng txt)这个参数需要与GUI Skin Custom Style中的Name保持一直。
+        GUI.Button(new Rect(0, 0, 700, 700), texture2d_btn, btnGuiSkin.GetStyle("Btn1"));
+    }
+
+}
