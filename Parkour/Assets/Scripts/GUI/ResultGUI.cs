70bfc15a1bc889038f1744b9bf551ac662d75cb1
lybglory <lybglory@outlook.com>
2017-07-16 11:58:18 +0800

    实现金币统计功能，重新开始游戏btn，退出游戏btn交互

--- Parkour/Assets/Scripts/GUI/ResultGUI.cs
+++ Parkour/Assets/Scripts/GUI/ResultGUI.cs
@@ -11,7 +11,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;//异步加载命名空间
 public class ResultGUI : MonoBehaviour {
     public Texture2D TextureRedDiamond;
     public Texture2D TextureBlueDiamond;
@@ -19,11 +19,24 @@ public class ResultGUI : MonoBehaviour {
     public GUISkin ResultGUISkin;
     private float XcenterPosition;
     private float YcenterPosition;
-    private float _tempShifting=0;          //局部变量
-    private bool _isSHowShifting=false;     //是否显示
+    private float _tempShifting=0;                    //局部里程变量
+    private bool _isSHowShifting=false;               //是否显示总里程
+    private float _tempDiamondNumber = 0;             //局部宝石变量
+    private bool _isSHowDiamondNumber = false;        //是否显示总吃到的红宝石
+    public string AgainSceneStr;                      //重新开始关卡名称
+    private bool IsTryAgain;                          //是否重新开始
+    public Texture2D TextureTryAgainBtn;              //重新开始btn贴图
+    private float TryAgainBtnHoriPosition;            //重新开始bt水平位置
+    private float TryAgainBtnVeriticalPosition;       //重新开始btn垂直问题
+    public Texture2D TextureCancel;                   //取消btn贴图
+    private float CancelBtnHoriPosition;              //取消bt水平位置
+    private float CancelBtnVeriticalPosition;         //取消btn垂直问题
+    private bool IsCancel;
+
+
 
     private void OnGUI()
-    {   //需要给GUI的皮肤赋值。字体的大小才会生效
+    {   //需要给GUI的皮肤赋值。GUI.Label字体的大小才会生效
         GUI.skin = ResultGUISkin;
         //绘制背景贴图,new Rect（屏幕中间的X轴position，Y轴position，贴图的长度，贴图的宽度）
         GUI.DrawTexture(new Rect(XcenterPosition, YcenterPosition, TextureResultBg.width,TextureResultBg.height), TextureResultBg);
@@ -33,7 +46,14 @@ public class ResultGUI : MonoBehaviour {
         if (_isSHowShifting) {
             GUI.Label(new Rect(700, 250, 159, 200), _tempShifting.ToString());
         }
-        
+        //绘制红宝石贴图
+        GUI.DrawTexture(new Rect(550, 300, 50, 50), TextureRedDiamond);
+        if (_isSHowDiamondNumber) {
+            GUI.Label(new Rect(700, 300, 150, 50), _tempDiamondNumber.ToString());
+        }
+        IsTryAgain=GUI.Button(new Rect(TryAgainBtnHoriPosition-200,TryAgainBtnVeriticalPosition-150, TextureTryAgainBtn.width, TextureTryAgainBtn.height),"", ResultGUISkin.GetStyle("Btn_TryAgain"));
+        IsCancel = GUI.Button(new Rect(CancelBtnHoriPosition + 200, CancelBtnVeriticalPosition - 150, TextureCancel.width, TextureCancel.height), "", ResultGUISkin.GetStyle("Btn_Cancel"));
+
 
     }
     // Use this for initialization
@@ -43,14 +63,28 @@ public class ResultGUI : MonoBehaviour {
         YcenterPosition = GlobalManager.GetTexturePosition(TextureResultBg,false);
         GlobalManager.Shifting = 20;//临时定义全局里程，测试用
 
+        //获取重新开始btn贴图中央位置
+        TryAgainBtnHoriPosition = GlobalManager.GetTexturePosition(TextureTryAgainBtn, true);
+        TryAgainBtnVeriticalPosition = GlobalManager.GetTexturePosition(TextureTryAgainBtn, false);
+        CancelBtnHoriPosition = GlobalManager.GetTexturePosition(TextureCancel, true);
+        CancelBtnVeriticalPosition = GlobalManager.GetTexturePosition(TextureCancel, false);
+
         StartCoroutine("ShowShifting");//执行协程
+        StartCoroutine("ShowDiamondsNumber");//执行协程
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {   //一旦获取到istryAgain为true
+        if (IsTryAgain) {
+            SceneManager.LoadSceneAsync("2_LevelOne");
+        }
+        //退出游戏
+        if (IsCancel) {
+            Application.Quit();
+            Debug.Log("退出游戏");
+        }
+    }
 
     /// <summary>
     /// 协程。全局里程显示
@@ -60,7 +94,8 @@ public class ResultGUI : MonoBehaviour {
     {   //先等待5s
         yield return new WaitForSeconds(0.5f);
         while (true) {
-            if (_tempShifting >= GlobalManager.Shifting-1)
+            
+            if (_tempShifting >=GlobalManager.Shifting-1)
             {
                 StopCoroutine("ShowShifting");
             }
@@ -72,7 +107,26 @@ public class ResultGUI : MonoBehaviour {
             _isSHowShifting = false;
             yield return new WaitForSeconds(0.2f);
         }
-
-
     }
+
+    /// <summary>
+    /// 协程，显示红宝石数量
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ShowDiamondsNumber() {
+        yield return new WaitForSeconds(0.5f);
+        while (true) {
+            //一旦累加的红宝石数量等于全局红宝石数量。就停止协程
+            if (_tempDiamondNumber>=GlobalManager.DiamondNum-1) {
+                StopCoroutine("ShowDiamondsNumber");
+            }
+            ++_tempDiamondNumber;
+            _isSHowDiamondNumber = true;
+            yield return new WaitForSeconds(0.2f);
+            _isSHowDiamondNumber = false;
+            yield return new WaitForSeconds(0.2f);   
+        }
+        
+    }
+
 }
