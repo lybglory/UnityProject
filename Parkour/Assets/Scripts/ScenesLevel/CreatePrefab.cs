eb1914933f79638c2bcd94165ef8f4da53c6ce52
lybglory <lybglory@outlook.com>
2017-07-19 23:42:02 +0800

    动态创建

---
+++ Parkour/Assets/Scripts/ScenesLevel/CreatePrefab.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 动态生成道具
+/// </summary>
+public class CreatePrefab : MonoBehaviour {
+
+    void CreateDiamondPrefab(System.Object[] objs) {
+        /*  8个参数：
+            原型
+            红宝石x坐标最小值，最大值，
+            y坐标(定值)
+            z坐标min、max。
+            红宝石数量
+            销毁时间
+         */
+        GameObject originObj = null;
+        int minX = 0;
+        int maxX = 0;
+        int y = 0;
+        int minZ = 0;
+        int maxZ = 0;
+        int diamondNum = 0;
+        int destroyTime = 0;
+        if (objs!=null) {
+            if (objs.Length==8) {
+                objs[0]= originObj;
+                objs[1] = minX;
+                objs[2] = maxX;
+                objs[3] = y;
+                objs[4] = minZ;
+                objs[5] = maxZ;
+                objs[6] = diamondNum;
+                objs[7] = destroyTime;
+
+            }
+        }
+        GameObject cloneObj = GameObject.Instantiate<GameObject>(originObj);
+        cloneObj.transform.position = new Vector3(GetRandomValue(minX,minZ), y, GetRandomValue(minZ,maxZ));
+        //销毁时间
+        Destroy(cloneObj, destroyTime);
+    }
+
+    /// <summary>
+    /// 获取随机值
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    private int GetRandomValue(int min,int max) {
+        int randomValue;
+        randomValue = Random.Range(min, max);
+        return randomValue;
+    }
+}
--- Parkour/Assets/Scripts/ScenesLevel/LevelOne_Manager.cs
+++ Parkour/Assets/Scripts/ScenesLevel/LevelOne_Manager.cs
@@ -14,6 +14,8 @@ using UnityEngine.SceneManagement;
 public class LevelOne_Manager : MonoBehaviour {
     private AudioSource aduioLevelOne;      //持有音频组件
     private string strGameOverScene;                //结算场景名称
+    public GameObject orinalObj;            //预制原型
+    public GameObject DynamicCreateObj;  //动态创建道具对象
     private void Awake()
     {
         strGameOverScene = "3_GameOver";
@@ -22,8 +24,13 @@ public class LevelOne_Manager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         //执行协程
-        StartCoroutine("Countdown");
         StartCoroutine("GameStateCheck");
+
+        //动态生成道具
+        //1秒创建一次
+        InvokeRepeating("DynamicCreatePrefab", 1, 1);
+
+
         aduioLevelOne = GameObject.Find("_LevelOneAudioManager/LevelOneAudio").GetComponent<AudioSource>();
         aduioLevelOne.Play();
         aduioLevelOne.loop = true;              //开启循环播放
@@ -70,4 +77,42 @@ public class LevelOne_Manager : MonoBehaviour {
             }
         }
     }
+
+    /// <summary>
+    /// 动态创建道具
+    /// </summary>
+    /// <param name="originalObj">道具预制原型</param>
+    /// <param name="minX">坐标X最小值</param>
+    /// <param name="maxX">坐标X最大值</param>
+    /// <param name="y">y坐标，定值</param>
+    /// <param name="minZ">坐标X最小值</param>
+    /// <param name="maxZ">坐标Z最大值</param>
+    /// <param name="destoryTime">销毁时间</param>
+    /// <param name="diamondNum">克隆数量</param>
+    private void DynamicCreatePrefab(GameObject originalObj,int minX,int maxX,int y, int minZ,int maxZ, int destroyTime, int diamondNum) {
+        /*  8个参数：
+            红宝石x坐标最小值，最大值，
+            z坐标min、max。
+            y坐标(定值)
+            红宝石数量
+            销毁时间
+         */
+        System.Object[] Objs = new System.Object[8];
+        Objs[0] = originalObj;
+        Objs[1] = minX;
+        Objs[2] = maxX;
+        Objs[3] = y;
+        Objs[4] = minZ;
+        Objs[5] = maxZ;
+        Objs[6] = diamondNum;
+        Objs[7] = destroyTime;
+
+        //发送数据
+        DynamicCreateObj.SendMessage("CreateDiamondPrefab",SendMessageOptions.DontRequireReceiver);
+    }
+
+    /// <summary>
+    /// 动态创建所有道具
+    /// </summary>
+    private void DynamicCreateAllObj() { }
 }
