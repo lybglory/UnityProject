f065a22e9408144c9c61e3a8063097c9c49834e2
lybglory <lybglory@outlook.com>
2017-10-13 00:31:10 +0800

    实现障碍物的上升、下降
    踩到倒刺，结束游戏

---
+++ Parkour/Assets/Scripts/ScenesLevel/CheckCollision.cs
@@ -0,0 +1,26 @@
+﻿/***
+*	title:跑酷项目开发
+*	[副标题]
+*	Description：
+*	[描述]
+*	Date：2017
+*	Version：0.1
+*	Modify Recoder：
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour {
+    private string plName="Hero";
+
+    private void Start()
+    {
+    }
+    private void OnCollisionEnter(Collision collision)
+    {   //一旦障碍物碰到英雄，游戏结束
+        if (collision.gameObject.name.Equals(plName)) {
+            GlobalManager.GlGameState = EnumGameState.End;
+        }
+    }
+}
---
+++ Parkour/Assets/Scripts/ScenesLevel/ThornUpDownMoving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 刺道具上升或下降
+/// </summary>
+public class ThornUpDownMoving : MonoBehaviour {
+    /// <summary>
+    /// 移动速度
+    /// </summary>
+    private float _moveSpeed=0.01f;
+    /// <summary>
+    /// 标志位，控制上升移动还是下降移动
+    /// </summary>
+    private int _flag=1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.y > -4.7f) {
+            _flag = -1;//乘以负数：下降
+        } else if (this.transform.position.y <-6.2f) {
+            _flag = 1;//乘以正数:上升
+        }
+        this.transform.Translate(Vector3.up*_flag * _moveSpeed,Space.World);
+    }
+}
