c618adfc773f24b9c0a496608efbd1f77f418274
lybglory <lybglory@outlook.com>
2017-10-27 23:21:42 +0800

    重写一个相机跟随脚本

---
+++ Parkour/Assets/Scripts/Global/SmoothFollow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 自己写一个相机跟随
+/// </summary>
+public class SmoothFollow : MonoBehaviour {
+
+
+    // The target we are following
+    public Transform Target;
+    // The distance in the x-z plane to the target
+    public float distance = 10;
+    // the height we want the camera to be above the target
+    public float height = 5;
+    // How much we 
+    public float heightDamping = 2;
+    public float rotationDamping = 3;
+
+    // Place the script in the Camera-Control group in the component menu
+    [AddComponentMenu("Camera-Control/Smooth Follow")]
+
+
+void LateUpdate()
+    {
+        // Early out if we don't have a target
+        if (!Target)
+        {
+            return;
+        }
+
+        // Calculate the current rotation angles
+        float wantedRotationAngle = Target.eulerAngles.y;
+        float wantedHeight = Target.position.y + height;
+
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        // Damp the rotation around the y-axis
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        transform.position = Target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight,transform.position.z);
+
+        // Always look at the target
+        transform.LookAt(Target);
+    }
+    
+}
