98d8827b0e59cf591281e9d08d13ee6ac9b734e6
lybglory <lybglory@outlook.com>
2017-10-28 17:09:49 +0800

    动态创建道具，多加入一个数组元素
    最后一个数组元素用来表示是南桥还是北桥

--- Parkour/Assets/Scripts/ScenesLevel/CreatePrefab.cs
+++ Parkour/Assets/Scripts/ScenesLevel/CreatePrefab.cs
@@ -8,13 +8,14 @@ using UnityEngine;
 public class CreatePrefab : MonoBehaviour {
 
     void CreateDiamondPrefab(System.Object[] objs) {
-        /*  8个参数：
+        /*  9个参数：
             原型
             红宝石x坐标最小值，最大值，
             y坐标(定值)
             z坐标min、max。
             克隆数量
             销毁时间
+            是否是南桥道具参考对象
          */
         GameObject originObj=null;
         float minX = 0;
@@ -24,8 +25,9 @@ public class CreatePrefab : MonoBehaviour {
         float maxZ = 0;
         int cloneNum = 0;
         float destroyTime = 0;
+        bool IsSourth = true;
         if (objs!=null) {
-            if (objs.Length==8) {
+            if (objs.Length==9) {
                 originObj= (GameObject)objs[0];
                 minX= System.Convert.ToSingle(objs[1]);
                 maxX= System.Convert.ToSingle(objs[2]);
@@ -34,6 +36,7 @@ public class CreatePrefab : MonoBehaviour {
                 maxZ = System.Convert.ToSingle(objs[5]); ;
                 cloneNum = System.Convert.ToInt32(objs[6]);
                 destroyTime = System.Convert.ToSingle(objs[7]);
+                IsSourth = (bool)objs[8];
 
             }
         }
@@ -41,7 +44,14 @@ public class CreatePrefab : MonoBehaviour {
         for (int i = 0; i < cloneNum; i++)
         {
             GameObject cloneObj = (GameObject)Instantiate(originObj);
-            cloneObj.transform.position = new Vector3(GetRandomValue(minX, maxX), y, GetRandomValue(minZ, maxZ));
+            if (IsSourth)
+            {   //南桥道具创建的位置
+                cloneObj.transform.position = new Vector3(GetRandomValue(minX, maxX), y, GetRandomValue(minZ, maxZ));
+            }
+            else {
+                //北桥坐标系变了
+                cloneObj.transform.position = new Vector3(GetRandomValue(minZ, maxZ),  y, GetRandomValue(minX, maxX));
+            }
             Debug.Log("minZ=" + minZ + ";maxZ=" + maxZ + ";创建出来道具的z轴坐标：" + cloneObj.transform.position.z);
             //销毁时间
             Destroy(cloneObj, destroyTime);
