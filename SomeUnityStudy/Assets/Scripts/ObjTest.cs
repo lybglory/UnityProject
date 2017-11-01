2f60df40fb8fca33afbb4e0867f771ec4ca09042
lybglory <lybglory@outlook.com>
2017-11-01 23:18:40 +0800

    DontDestroyOnLoad方法的应用
    加载场景的时候，目标物体不被销毁

---
+++ SomeUnityStudy/Assets/Scripts/ObjTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ObjTest : MonoBehaviour {
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+    private void OnGUI()
+    {
+        if (GUILayout.Button("载入新场景",GUILayout.Width(200),GUILayout.Height(200))) {
+            
+            SceneManager.LoadScene("gui");
+        }
+    }
+}
