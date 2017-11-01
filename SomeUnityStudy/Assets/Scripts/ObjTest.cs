ca8a7b80a20f22283c44a7cf55562c2c327e296b
lybglory <lybglory@outlook.com>
2017-11-01 23:29:39 +0800

    DontDestroyOnLoad测试代码

--- SomeUnityStudy/Assets/Scripts/ObjTest.cs
+++ SomeUnityStudy/Assets/Scripts/ObjTest.cs
@@ -5,13 +5,13 @@ using UnityEngine.SceneManagement;
 public class ObjTest : MonoBehaviour {
 
     private void Start()
-    {
+    {   //加载场景的时候，目标物体不被销毁
         DontDestroyOnLoad(this.gameObject);
     }
     // Use this for initialization
     private void OnGUI()
     {
-        if (GUILayout.Button("载入新场景",GUILayout.Width(200),GUILayout.Height(200))) {
+        if (GUILayout.Button("载入新场景")) {
             
             SceneManager.LoadScene("gui");
         }
