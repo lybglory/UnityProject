54be7b438da0fe405b61c48308675d1d9a1b2abd
lybglory <lybglory@outlook.com>
2017-11-02 23:22:11 +0800

    脚本生命周期的测试
    重点是OnEnable函数和OnStart函数的调用

---
+++ SomeUnityStudy/Assets/Scripts/MonobehaviorTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonobehaviorTest : MonoBehaviour {
+    private void Awake(){
+        Debug.Log("I'm Awake()");
+    }
+
+    private void OnEnable(){
+        Debug.Log("I'm OnEnable()");
+        StartCoroutine("IETest");//开始协程
+    }
+    // Use this for initialization
+    void Start () {
+        Debug.Log("I'm Start()");
+        //StartCoroutine("IETest");
+	}
+	
+	void Update () {
+        Debug.Log("I'm Update()");
+    }
+
+    private void LateUpdate(){
+        Debug.Log("I'm LateUpdate()");
+    }
+
+    private void OnGUI(){
+        Debug.Log("I'm OnGUI()");
+    }
+    private void OnDisable(){
+        Debug.Log("I'm OnDisable()");
+        StopCoroutine("IETest");//停止协程
+    }
+
+    private void OnDestroy(){
+        Debug.Log("I'm OnDestroy()");
+    }
+
+    IEnumerator IETest() {
+        yield return new WaitForSeconds(1f);
+        while (true){
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log("协程");
+        }
+    }
+}
---
+++ SomeUnityStudy/Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    void Update()
+    {
+        Debug.Log("I'm Update()");
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("按下A键");
+            this.gameObject.GetComponent<MonobehaviorTest>().enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("按下D键");
+            this.gameObject.GetComponent<MonobehaviorTest>().enabled = false;
+        }
+    }
+}
