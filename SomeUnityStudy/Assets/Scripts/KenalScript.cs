899b808ef053ca19655820e6a56f7a94b1c9c820
lybglory <lybglory@outlook.com>
2017-11-03 23:34:03 +0800

    TimeScale实现暂停效果

--- SomeUnityStudy/Assets/Scripts/KenalScript.cs
+++ SomeUnityStudy/Assets/Scripts/KenalScript.cs
@@ -11,10 +11,19 @@ public class KenalScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //一旦按下空格键，Cube游戏对象停止旋转
+        if (Input.GetKeyDown(KeyCode.Space )) {
+            Time.timeScale = 0;
+        }
+        Debug.Log("Time.time=" + Time.time);
+        Debug.Log("Time.fixedDeltaTime="+ Time.fixedDeltaTime);//0.02s
 	}
     private void FixedUpdate()
     {
-        Debug.Log("FixedUpdate.time="+Time.deltaTime);
+        this.transform.Rotate(Vector3.up, Space.World);
+    }
+    private void LateUpdate()
+    {
+        this.transform.Rotate(Vector3.up, Space.World);
     }
 }
