4c423c5a031813b97768cf8b3497c4165ea7be7b
lybglory <lybglory@outlook.com>
2017-11-04 23:53:12 +0800

    同步加载

---
+++ SomeUnityStudy/Assets/Scripts/StartScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartScene : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("下一关"))
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+}
