6dc47a7db7366126a77527beb41004a6026c1dae
lybglory <lybglory@outlook.com>
2017-11-09 23:41:59 +0800

    开始场景加载

---
+++ SomeUnityStudy/Assets/Scripts/SceneScript/StartScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartScene : SceneBaseCtr
+{
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("下一关"))
+        {
+            GlobalParamater.GlScene = EnumScene.LevelOneScene;
+            LoadScene(GlobalParamater.GlScene);
+        }
+    }
+}
