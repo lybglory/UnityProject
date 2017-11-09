029c1864dd4fdf41c5c96294a120961412d9f2c8
lybglory <lybglory@outlook.com>
2017-11-09 23:42:20 +0800

    实现通用

---
+++ SomeUnityStudy/Assets/Scripts/SceneScript/GlobalParamater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 场景枚举
+/// </summary>
+public enum EnumScene {
+    StartScene,
+    LoadScene,
+    LevelOneScene,
+    LevelTwoScene
+}
+public class GlobalParamater : MonoBehaviour {
+    /// <summary>
+    /// 全局静态枚举变量
+    /// </summary>
+    public static EnumScene GlScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
