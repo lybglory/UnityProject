c967527f0bb4171c90abc3c620500354f54b3243
lybglory <lybglory@outlook.com>
2017-11-09 23:43:01 +0800

    实现进入任何一个场景都要加载一个过渡场景

---
+++ SomeUnityStudy/Assets/Scripts/SceneScript/AsyncLoad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsyncLoad : MonoBehaviour {
+    /// <summary>
+    /// Slider句柄
+    /// </summary>
+    public Slider loadSlider;
+
+
+    private void Start()
+    {
+        StartCoroutine("LoadNextScene");
+    }
+
+    private void Update()
+    {
+        loadSlider.value += Time.deltaTime;
+        if (loadSlider.value<=0) {
+            loadSlider.value += Time.deltaTime;
+        }
+        Debug.Log("loadSlider.value" + loadSlider.value+";时间："+Time.time);
+        
+    }
+
+    IEnumerator LoadNextScene() {
+        yield return new WaitForSeconds(1f);
+        AsyncOperation asyncOperaNext = SceneManager.LoadSceneAsync(GlobalParamater.GlScene.ToString());
+        yield return asyncOperaNext;
+    }
+}
---
+++ SomeUnityStudy/Assets/Scripts/SceneScript/SceneBaseCtr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBaseCtr : MonoBehaviour {
+
+    private float flProgress = 0;
+    private AsyncOperation asyoper;
+
+    public void LoadScene(EnumScene loadScene) {
+        asyoper=SceneManager.LoadSceneAsync(EnumScene.LoadScene.ToString());
+        if (asyoper.isDone) {
+            GlobalParamater.GlScene=loadScene;
+        }
+
+        
+
+    }
+}
