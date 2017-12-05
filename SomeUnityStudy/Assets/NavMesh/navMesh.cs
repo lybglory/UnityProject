d5488ed6b223f688c342472d8760178b8008f16c
lybglory <lybglory@outlook.com>
2017-12-05 23:09:06 +0800

    NavMeshAgent实验

---
+++ SomeUnityStudy/Assets/NavMesh/navMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;//NavMeshAgent命名空间
+
+public class navMesh : MonoBehaviour {
+    private NavMeshAgent NMAgent;
+    /// <summary>
+    /// 目标位置
+    /// </summary>
+    public Transform TransTarget;
+	// Use this for initialization
+	void Start () {
+        NMAgent=transform.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (NMAgent&TransTarget) {
+            NMAgent.SetDestination(TransTarget.position);
+        }
+	}
+}
