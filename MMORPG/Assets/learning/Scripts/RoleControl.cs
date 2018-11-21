8147332f70ae902ae6574d51e5a805794c74feb8
lybglory@outlook.com <YUbin071214>
2018-11-21 13:45:42 +0800

    目标点和相机画出射线

---
+++ MMORPG/Assets/learning/Scripts/RoleControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleControl : MonoBehaviour {
+    /// <summary>
+    /// 角色控制器
+    /// </summary>
+    private CharacterController characterController;
+    /// <summary>
+    /// 射线
+    /// </summary>
+    private Ray ray;
+    /// <summary>
+    /// 目标位置
+    /// </summary>
+    private Vector3 targetPosition;
+    RaycastHit raycastHit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0)) {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out raycastHit)){
+                if (raycastHit.collider.gameObject.name.Equals("Plane",System.StringComparison.CurrentCultureIgnoreCase)) {
+                    targetPosition = raycastHit.point;
+                }
+            }
+        }
+
+        if (targetPosition!=Vector3.zero) {
+            Debug.DrawLine(Camera.main.transform.position, targetPosition,Color.red);
+        }
+        
+    }
+}
